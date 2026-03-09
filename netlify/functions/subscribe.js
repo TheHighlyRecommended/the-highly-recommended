4010159e8618bc8b2645e36c8dd1be0acae8fd00
TheHighlyRecommended <hello@thehighlyrecommended.com>
2026-03-09 18:00:59 +1100

    Create subscribe.js

---
+++ netlify/functions/subscribe.js
@@ -0,0 +1,31 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { email } = JSON.parse(event.body);
+  if (!email) {
+    return { statusCode: 400, body: JSON.stringify({ error: 'Email required' }) };
+  }
+
+  const response = await fetch('https://api.kit.com/v4/forms/9181918/subscribers', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Kit-Api-Key': process.env.KIT_API_KEY
+    },
+    body: JSON.stringify({ email_address: email })
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    return { statusCode: 500, body: JSON.stringify({ error: data }) };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({ success: true })
+  };
+};
